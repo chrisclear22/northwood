f67369510725c6de5b6931dfd0cecd8787796109
Danny G Smith <dgs@riskiii.com>
2015-05-19 15:16:19 -0500

    removed footer text

--- footer.php
+++ footer.php
@@ -12,9 +12,7 @@
 
 	<footer id="colophon" class="site-footer col-md-12" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'northwood' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'northwood' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'northwood' ), 'northwood', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+         
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
