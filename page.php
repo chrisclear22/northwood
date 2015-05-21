a39f795da782b02f0e5c45cf964605bf40bb565d
Danny G Smith <dgs@riskiii.com>
2015-05-21 17:39:19 -0500

    working on responsive

--- page.php
+++ page.php
@@ -12,14 +12,12 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main col-md-8" role="main">
+	<div id="primary" class="content-area  col-md-8">
+		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
-
-				
 
 			<?php endwhile; // end of the loop. ?>
 
