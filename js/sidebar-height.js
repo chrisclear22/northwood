f7729682f2efd54ee11c28b83ea772d0aa87c07a
Danny G Smith <dgs@riskiii.com>
2015-05-22 10:42:52 -0500

    responsive

--- js/sidebar-height.js
+++ js/sidebar-height.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function($) {
-    $("#secondary").css({'height':($(".content-area").height()+'px')});
+
+    if ( $(".content-area").height() > $("#secondary").height() ) {
+        $("#secondary").css({'height': ($(".content-area").height() + 'px')});
+    } else {
+        $(".content-area").css({'height': ($("#secondary").height() + 'px')});
+    }
 });
